b564ffe4b42a14bb59deb4413ac59f1407b6e769
stasjem <stasjem@gmail.com>
2024-05-15 15:59:52 +0400

    Add files via upload

---
+++ lesson1_21_09_2023.py
@@ -0,0 +1,10 @@
+
+
+name = 'это моя строка'
+name2 = 'это новая строка'
+print(name[6]) #1
+print(name[:-1]) #2
+print(name[2:4]) #3
+print(name[::-1]) #4
+print(len(name)) #5
+print(name + " " + name2) #6
