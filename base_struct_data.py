5d98e98cf8bd19fa13a6a454756e6f4cee47979a
stasjem <stasjem@gmail.com>
2024-05-17 18:16:38 +0400

    Create base_struct_data.py

---
+++ base_struct_data.py
@@ -0,0 +1,29 @@
+name = 'Это моя первая программа'
+z = 20
+x = 10
+numbers = [1, 2, 3]
+
+# Задача «Длина слова».
+print(len(name))
+
+# Задача «Суммы и разности».
+print(z + x)
+print(z - x)
+print(str(z + x))
+print(str(z - x))
+
+# Задача «Суммы и разности».
+average = sum(numbers) / len(numbers)
+print(str(average))
+
+# Задача «Простые строчки».
+a = 'Вторник'
+b = 'Понедельник'
+print(b + ', ' + a)
+
+# Задача «Сложная формула».
+a = 1
+b = 2
+c = 4
+f = (a * b) + (a * c)
+print(f ** 3 / 2)
