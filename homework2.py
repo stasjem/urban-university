20de9febb06c08316c3932d1bd3648c9bcef4bc7
stasjem <stasjem@gmail.com>
2024-05-17 20:36:51 +0400

    homework2.py

---
+++ homework2.py
@@ -0,0 +1,11 @@
+name = 'Stanislav'
+print('Name: ' + name)
+
+age = 20
+print('Age: ' + str(age))
+
+age += 1
+print('New Age: ' + str(age))
+
+is_student = True
+print('Is Student: ' + str(is_student))
