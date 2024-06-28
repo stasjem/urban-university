1560c1c9da5d9e88a71d88e27b9b796747e5a3c7
user <stasjem@gmail.com>
2024-06-28 19:14:56 +0400

    Merge remote-tracking branch 'origin/main'

--- module_5_2.py
+++ module_5_2.py
@@ -27,5 +27,5 @@ class House:
 my_house = House()
 print(f"Начальное количество этажей: {my_house.numberOfFloors}")
 
-my_house.setNewNumberOfFloors(5)
-my_house.setNewNumberOfFloors(9)
+my_house.setNewNumberOfFloors(10)
+my_house.setNewNumberOfFloors(20)
